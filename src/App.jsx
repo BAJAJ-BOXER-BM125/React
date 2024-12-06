2c223cc7be1a1cbd6f1f88c864d15cdbb24c1fe9
Cybersport <Cybersport@26kadr.internal>
2024-12-06 10:29:39 +0300

    5  day

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,11 @@ import subtract from './img/Subtract.svg'
 import light from './img/LIGHT.svg'
 import screenshot from './img/Screenshot.svg'
 import arrow from './img/Arrow.svg'
-import frame from './img/Frame 7.svg'
 import shirt from './img/shirt.svg'
 import arrowleft from './img/ArrowLeft.svg'
 import arrowright from './img/ArrowRight.svg'
 import framm from './img/framm.svg'
 import men from './img/men.svg'
-import rt from './img/rt.svg'
-import text from './img/text.svg'
 import image from './img/image.svg'
 import image2 from './img/image2.svg'
 import arroww from './img/Arroww.svg'
@@ -34,6 +31,11 @@ import pt from './img/pt.svg'
 import oo from './img/oo.svg'
 import ll from './img/ll.svg'
 import home from './img/home.svg'
+import numerro from './img/Numerro.svg'
+import vr from './img/vr.svg'
+import v from './img/V.svg'
+import fb from './img/FB.svg'
+import rrr from './img/rrr.svg'
 function App() {
   return (
     <div className="App bg">
@@ -54,39 +56,43 @@ function App() {
         <div className='text text-[30px] items-start flex font-bold ml-[80px] mt-[-100px]'>
           <p> Spring season</p>
         </div>
-        <div className='text text-[20px] items-start flex font-bold ml-[1500px] mt-[-60px]'>
+        <div className='text text-[20px] items-start flex font-bold ml-[1700px] mt-[-60px]'>
           <p>VIEW COLLECTION</p>
           <img className='' src={arrow} alt="" />
         </div>
-        <div className='app1'>
-          <header className='box1 text text-[50px] items-center flex justify-between px-20 py-10  h-[20px] gap-[90px] mt-[85px]'>
-            <img className='ml-[200px]' src={frame} alt="" />
-          </header>
-        </div>
-        <div className='flex w-full'>
+        <div className='box items-center flex justify-between px-28 py-10 mt-[78px]'>
+          <img src={numerro} alt=""/>
+          <img src={vr} alt=""/>
+          <img src={v} alt=""/>
+          <img src={fb} alt=""/>
+          <img src={rrr} alt=""/>
+          </div>
+
+
+        <div className='flex w-full '>
           <img className='ml-20' src={arrowleft} alt="" />
           <img className=' h-[754px] gap-[90px] mt-[60px] px-20 py-10 ' src={shirt} alt="w-1/2" />
           <img className='mr-9' src={arrowright} alt="" />
           <img src={framm} alt="" className='w-1/2' />
         </div>
-        <div className='text1 text-[30px] items-start flex font-bold ml-[430px] mt-[-210px]'>
+        <div className='text1 text-[30px] items-start flex font-bold ml-[450px] mt-[-210px]'>
           <p> Basic Shirt</p>
         </div>
-        <div className='text1 text-[30px] items-start flex font-bold ml-[470px] mt-[-2px]'>
+        <div className='text1 text-[30px] items-start flex font-bold ml-[490px] mt-[-2px]'>
           <p>€49</p>
         </div>
         <div className='text text-[30px] items-start flex font-bold ml-[1095px] mr-[-100px] mt-[-1px]'>
-          <button className='bg-black text-white text-[25px] px-4 py-4 rounded'>Shop Shirts</button>
+          <button className='bg-black text-white text-[25px] px-4 py-4 rounded-[10px]'>Shop Shirts</button>
         </div>
         <div className='flex w-full '>
-          <img className=' box3 h-[636px] w-[596px]  mt-[170px] mr-[190px] ml-[350px] px-25 py-30 rounded-[10px] ' src={men} alt="" />
-          <header className='box2 text2 text-[60px] items-center flex justify-between px-30 py-30  h-[636px] w-[-596px] r-[8px] gap-[0px] mt-[170px]  mr-[190px] ml-[-90px] rounded'>
-            <img className='mt-[-260px] ' src={text} alt="" />
-            <img className='mt-[550px] ml-[-190px]' src={rt} alt="" />
-          </header>
-        </div>
-        <div className='text text-[-1px] items-start flex font-bold ml-[400px] mt-[-100px] mr-[200px]'>
-          <button className='bg-black text-white text-[18px] px-4 py-4 rounded'>Shop Sweaters</button>
+          <img className=' box3 h-[636px] w-[596px]  mt-[170px] mr-[190px] ml-[420px] px-25 py-30 rounded-[10px] ' src={men} alt="" />
+         </div>
+         <div className=' bg1 mt-[-635px]  w-[650px] h-[636px] ml-[1050px] mb-[600px] rounded-[10px]'></div>
+          <p className='text-[40px] items-start flex font-bold ml-[1100px] mt-[-1200px] py-3 w-[516px]  h-[278px]'>“ First Light promises quality, timeless designs and with our mission it is truly a buy it for life purchase. “</p>
+          <p className='text-[20px] items-start flex font-bold ml-[1100px] mt-[200px] py-3 '>— Guera Latissa, Creative Director</p>
+         
+        <div className='text text-[-1px] items-start flex font-bold ml-[460px] mt-[-50px] mr-[200px]'>
+          <button className='bg-black text-white text-[18px] px-4 py-4 rounded-[10px]'>Shop Sweaters</button>
         </div>
         <div className='flex w-full items-center flex justify-between  px-3 py-3'>
           <img className=' h-[974px] w-[757px] mt-[-1px] mr-[-100px] ml-[360px] px-20 py-20 rounded-l-md' src={image} alt="" />
@@ -101,7 +107,7 @@ function App() {
           <img className='' src={arroww} alt="" />
         </div>
         <header className='box6 items-center flex justify-between px-3 py-3'>
-          <p className=' front-bold text-[17px] text-[50px] flex font-bold ml-[830px] mt-[130px]'> <b>Hot off the press</b></p>
+          <p className=' front-bold text-[17px] text-[50px] flex font-bold ml-[840px] mt-[130px]'> <b>Hot off the press</b></p>
         </header>
         <div className="mt-5 flex  items-center justify-center"></div>
         <div className='w-[1260px] h-[286px] ml-[450px]  border-2 rounded-md flex '></div>
@@ -142,40 +148,43 @@ function App() {
           <img src={up} alt="" />
         </div>
 
-        <div className='box3 front-bold text-[17px] text-[50px] flex font-bold ml-[780px] mt-[130px]'>
+        <div className=' front-bold text-[17px] text-[50px] flex font-bold ml-[790px] mt-[130px]'>
           <p>Women — Must Haves</p>
         </div>
         <div className='flex w-full rounded-md '>
-          <img className=' box5 h-[420px] w-[313px]  mt-[50px] mr-[190px] ml-[450px] px-25 py-30 ' src={model} alt="" />
+          <img className=' box5 h-[420px] w-[313px]  mt-[50px] mr-[190px] ml-[390px] px-25 py-30 ' src={model} alt="" />
           <img className=' box5 h-[420px] w-[313px]  mt-[50px] mr-[-290px] ml-[-150px] px-25 py-30 ' src={model1} alt="" />
           <img className=' box5 h-[420px] w-[313px]  mt-[50px] mr-[-290px] ml-[330px] px-25 py-30  ' src={model2} alt="" />
           <img className=' box5 h-[420px] w-[313px]  mt-[50px] mr-[-290px] ml-[330px] px-25 py-30  ' src={model3} alt="" />
           <img className='ml-[-1160px] h-[56px] w-[56px] mt-[220px]' src={buttonleft} alt="" />
           <img className='ml-[1280px] h-[56px] w-[56px] mt-[220px]' src={buttonright} alt="" />
         </div>
-        <div className='text1 w-[256px] h-[26px] text-[16px] items-start flex font-bold ml-[470px] mt-[30px]'>
+        <div className='text1 w-[256px] h-[26px] text-[16px] items-start flex font-bold ml-[410px] mt-[30px]'>
           <p> Basic Shirt</p>
         </div>
-        <div className='text1 text-[16px] items-start flex font-bold ml-[710px] mt-[-30px]'>
+        <div className='text1 text-[16px] items-start flex font-bold ml-[650px] mt-[-30px]'>
           <p>€49</p>
         </div>
 
-        <div className='text1 w-[256px] h-[26px] text-[16px] items-start flex font-bold ml-[850px] mt-[-25px]'>
+        <div className='text1 w-[256px] h-[26px] text-[16px] items-start flex font-bold ml-[770px] mt-[-25px]'>
           <p>Beige Jumper</p>
         </div>
-        <div className='text1 text-[16px] items-start flex font-bold ml-[1050px] mt-[-25px]'>
+        <div className='text1 text-[16px] items-start flex font-bold ml-[1005px] mt-[-25px]'>
           <p>€99</p>
         </div>
 
-        <div className='text1 w-[256px] h-[26px] text-[16px] items-start flex font-bold ml-[1200px] mt-[-25px]'>
+        <div className='text1 w-[256px] h-[26px] text-[16px] items-start flex font-bold ml-[1120px] mt-[-25px]'>
           <p>Signature Jacket</p>
         </div>
-        <div className='text1 text-[16px] items-start flex font-bold ml-[1405px] mt-[-25px]'>
+        <div className='text1 text-[16px] items-start flex font-bold ml-[1350px] mt-[-25px]'>
           <p>€249</p>
         </div>
 
-        <div className='text1 w-[256px] h-[26px] text-[16px] items-start flex font-bold ml-[1550px] mt-[-25px]'>
+        <div className='text1 w-[256px] h-[26px] text-[16px] items-start flex font-bold ml-[1470px] mt-[-25px]'>
           <p>Navy Jumper</p>
+        </div>
+        <div className='text1 text-[16px] items-start flex font-bold ml-[1705px] mt-[-25px]'>
+          <p>€340</p>
         </div>
         <div className='box6 front-bold text-[17px] text-[50px] flex font-bold ml-[750px] mt-[100px]'>
           <p>We always have your back</p>
